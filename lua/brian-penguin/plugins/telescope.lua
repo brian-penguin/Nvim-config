f0cbdc40712e91d93e5678281a00c4ff6bc731ec
brian-penguin <btenggren@onemedical.com>
2024-02-16 18:59:49 -0500

    Make a small change to undo, telescope and which key
    I want to make this a little more cohesive

--- lua/brian-penguin/plugins/telescope.lua
+++ lua/brian-penguin/plugins/telescope.lua
@@ -1,41 +1,43 @@
 -- Telescope works as a tool for us to search through our project space
 return {
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        -- or                              , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        -- or
+        -- , branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' },
         keys = {
             {
                 "<leader>ff",
-                function() 
+                function()
                     require("telescope.builtin").find_files({
                         previewer = false,
                         shorten_path = true,
                         layout_strategy = "horizontal"
-                    }) 
+                    })
                 end,
-                desc = "Find Files" 
+                desc = "Find Files"
             },
             {
                 "<leader>pf",
-                function() 
+                function()
                     require("telescope.builtin").git_files({
                         previewer = false,
                         shorten_path = true,
                         layout_strategy = "horizontal"
-                    }) 
+                    })
                 end,
-                desc = "Find git Files" 
+                desc = "Find git Files"
             },
             {
                 "<leader>vh",
                 function() require("telescope.builtin").help_tags() end,
-                desc = "Help Tags" 
+                desc = "Help Tags"
             },
             {
-                "<leader>ps",
-                function() require("telescope.builtin").grep_string({search = vim.fn.input("Grep > ")}) end,
-                desc = "Project Search" 
+                "<leader>sp",
+                function() require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") }) end,
+                desc = "SearchProject"
             },
         },
     },
