75c094481ec67d160c16e9818ece55edf0831a94
brian-penguin <btenggren@onemedical.com>
2024-02-16 18:58:56 -0500

    Update some of the default settings

--- lua/brian-penguin/set.lua
+++ lua/brian-penguin/set.lua
@@ -17,7 +17,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
@@ -27,6 +27,6 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 80
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "100"
