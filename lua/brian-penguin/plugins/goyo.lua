5fef42645da73e6de46cc7c8042f477f70894a8b
brimatteng <btenggren20@gmail.com>
2026-01-15 15:21:27 -0500

    Add goyo and limelight for editing text mode

---
+++ lua/brian-penguin/plugins/goyo.lua
@@ -0,0 +1,12 @@
+return {
+  -- A distraction free writing mode!
+  -- :Goyo to turn on
+  -- :Goyo <WIDTH>x<HEIGHT> (can supply only width or x<HEIGHT> for sizes)
+  -- :Goyo! to turn off
+  "junegunn/goyo.vim",
+  -- focus on only the line under
+  -- :limelight to turn on
+  -- :limelight! to turn off
+  -- :limelight!! to toggle
+  "junegunn/limelight.vim"
+}
