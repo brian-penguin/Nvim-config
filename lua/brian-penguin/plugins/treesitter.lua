7d9ffe32824c6457a4bda8438d3da1a0a115de79
brian-penguin <tenggren@thoughtbot.com>
2026-01-22 08:51:40 -0500

    Update Treesitter config to remove custom config support
    Why this change is being made:
    - Treesitter no longer supports custom configs (I wasn't using teml
      anyways)
    
    What were the changes made to support this:
    - remove most of the config, make a bak file incase I need to revert
      something

--- lua/brian-penguin/plugins/treesitter.lua
+++ lua/brian-penguin/plugins/treesitter.lua
@@ -1,60 +1,50 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"andymass/vim-matchup",
-	},
-	build = ":TSUpdate",
-	lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-	init = function(plugin)
-		-- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-		-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-		-- no longer trigger the **nvim-treesitter** module to be loaded in time.
-		-- Luckily, the only things that those plugins need are the custom queries, which we make available
-		-- during startup.
-		require("lazy.core.loader").add_to_rtp(plugin)
-		require("nvim-treesitter.query_predicates")
-	end,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-			auto_install = true,
-
-			indent = {
-				-- TODO -> This might be why ruby is super janky with indenting
-				--  - trying setting it to false from true
-				enable = false,
-			},
-
-			highlight = {
-				-- `false` will disable the whole extension
-				enable = true,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = { "markdown" },
-			},
-
-			-- Use the matchup plugin to get better '%' jumping (no more jumping to comments in ruby)
-			matchup = {
-				enable = true,
-			},
-		})
-
-		local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-		treesitter_parser_config.templ = {
-			install_info = {
-				url = "https://github.com/vrischmann/tree-sitter-templ.git",
-				files = { "src/parser.c", "src/scanner.c" },
-				branch = "master",
-			},
-		}
-
-		vim.treesitter.language.register("templ", "templ")
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "andymass/vim-matchup",
+  },
+  version = false,
+  build = ":TSUpdate",
+  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+  init = function(plugin)
+    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+    -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+    -- Luckily, the only things that those plugins need are the custom queries, which we make available
+    -- during startup.
+    require("lazy.core.loader").add_to_rtp(plugin)
+  end,
+  opts_extended = { "ensure_installed" },
+  opts = {
+    indent = { enable = false }, -- This causes weird stuff to happen in ruby
+    highlight = { enable = true },
+    folds = { enable = true },
+    ensure_installed = {
+      "bash",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "ruby",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+    matchup = true
+  }
 }
